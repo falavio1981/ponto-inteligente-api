cf18d0c92e829dee749bd370a4d31eb07a6d2374
flaviohs@ciandt.com <flaviohs@lnxcit017522.cit>
2020-07-30 16:45:13 -0300

    Create Entities

---
+++ src/main/java/com/example/pontointeligente/enums/PerfilEnum.java
@@ -0,0 +1,6 @@
+package com.example.pontointeligente.enums;
+
+public enum PerfilEnum {
+    ROLE_ADMIN,
+    ROLE_USUARIO
+}
---
+++ src/main/java/com/example/pontointeligente/enums/TipoEnum.java
@@ -0,0 +1,10 @@
+package com.example.pontointeligente.enums;
+
+public enum TipoEnum {
+    INICIO_TRABALHO,
+    TERMINIO_TRABALHO,
+    INICIO_ALMOCO,
+    TERMINO_ALMOCO,
+    INICIO_PAUSA,
+    TERMINO_PAUSA
+}
